6f8ac20c99f98b2f3cae571d06084f6c5d51e090
rthomase422 <rthomase@live.unc.edu>
2022-04-21 22:36:03 -0400

    data

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const database = require('better-sqlite3')
 
 const logdb = new database('log.db')
-const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and 'access';`)
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and 'access';`)
 let row = stmt.get();
 
 if (row === undefined) {
