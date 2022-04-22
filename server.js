c11d433a04fb26d17e16ff0ba451f7ddef8ed13c
rthomase422 <rthomase@live.unc.edu>
2022-04-22 01:43:25 -0400

    maybe idk bro

--- server.js
+++ server.js
@@ -58,6 +58,24 @@ if (log != 'false') {
     app.use(morgan('FORMAT', { stream: WRITESTREAM }))
 }
 
+app.use((req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    };
+    const stmt = logdb.prepare(`INSERT INTO access (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
+    const data = stmt.run(logdata.remoteaddr), (logdata.remoteuser), (logdata.time), (logdata.method), (logdata.url), (logdata.protocol), (logdata.httpversion), (logdata.status), String(logdata.referrer),(logdata.useragent)
+    next();
+});
+
 
 const logging = (req, res, next) => {
     console.log(req.ip)
