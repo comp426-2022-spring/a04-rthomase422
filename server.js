73023bff7a6bd741273535a3a4b196c70bd689bb
rthomase422 <rthomase@live.unc.edu>
2022-04-22 02:12:45 -0400

    help

--- server.js
+++ server.js
@@ -10,53 +10,49 @@ const logdb = require('./database')
 
 
 //const md5 = require('md5')
-
-
 app.use(express.urlencoded({extended : true}));
 app.use(express.json)
 
 
 const args = require('minimist')(process.argv.slice(2))
+const port = args.port || process.env.PORT || 5555
+const debug = args.debug || false
+const log = args.log || false
 console.log(args)
 args['port']
 args['help']
 args['debug']
 args['log']
 
-const help = (`
-server.js [options]
 
---port	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535.
-
---debug	If set to true, creates endlpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error which throws 
-            an error with the message "Error test successful." Defaults to 
-            false.
-
---log		If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
-
---help	Return this message and exit.
-`)
 
 
 if (args.help || args.h) {
+    const help = (`
+    server.js [options]
+
+    --port	Set the port number for the server to listen on. Must be an integer
+                between 1 and 65535.
+
+    --debug	If set to true, creates endlpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                false.
+
+    --log		If set to false, no log files are written. Defaults to true.
+                Logs are always written to database.
+
+    --help	Return this message and exit.
+    `)
     console.log(help)
     process.exit(0)
 }
 
-const port = args.port || process.env.PORT || 5000
-const debug = args.debug || false
-const log = args.log || false
 
 const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%',port))
 });
-if (log != 'false') {
-    const WRITESTREAM = fs.createWriteStream('FILE', { flags: 'a' })
-    app.use(morgan('FORMAT', { stream: WRITESTREAM }))
-}
+
 
 app.use((req, res, next) => {
     let logdata = {
@@ -88,10 +84,15 @@ if(debug) {
     });
 }
 
+if (log != 'false') {
+    const WRITESTREAM = fs.createWriteStream('FILE', { flags: 'a' })
+    app.use(morgan('FORMAT', { stream: WRITESTREAM }))
+}
+
 //app.use(fs.writeFile('./access.log', morgan('combined'), {flag : 'a' }, (err, req, res, next) => {if (err) {console.error(err)} else {console.log() }))
 // maybe?
 
-app.get('/app/', (req, res, next) => {
+app.get('/app/', (req, res) => {
     res.status(200).end('OK')
     res.type('text/plain')
 
