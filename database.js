729f8261fe7faebeed1d727043184afe682c2fb2
rthomase422 <rthomase@live.unc.edu>
2022-04-24 14:58:42 -0400

    edit

--- database.js
+++ database.js
@@ -1,15 +1,17 @@
-"use strict";
+"use strict"
 
-const database = require('better-sqlite3')
+const database = require("better-sqlite3")
 
 const logdb = new database('log.db')
-const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
-let row = stmt.get();
+const stmt = logdb.prepare(
+  `SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
+)
+let row = stmt.get()
 
 if (row === undefined) {
-    console.log('Log database appears to be empty. Creating log database...')
+  console.log("Log database appears to be empty. Creating log database...")
 
-    const sqlInit = `
+  const sqlInit = `
         CREATE TABLE accesslog ( 
             id INTEGER PRIMARY KEY, 
             remoteaddr TEXT,
@@ -25,11 +27,10 @@ if (row === undefined) {
             
         )`
 
-    logdb.exec(sqlInit)
-    console.log('Database has been created.')
-
+  logdb.exec(sqlInit)
+  console.log("Database has been created.")
 } else {
-    console.log('Log database exists.')
+  console.log("Log database exists.")
 }
 
 module.exports = logdb
--- server.js
+++ server.js
@@ -1,45 +1,60 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 //const errorhandler = require('errorhandler')
-const morgan = require("morgan");
-const fs = require("fs");
-const logdb = require("./database.js");
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json);
+const morgan = require("morgan")
+const fs = require("fs")
+const logdb = require("./database.js")
 
-const args = require("minimist")(process.argv.slice(2));
-const port = args.port || process.env.PORT || 5555;
-const debug = args.debug || false;
-const log = args.log || true;
-//console.log(args)
-args[("port", "debug", "log", "help")];
+const args = require("minimist")(process.argv.slice(2))
+const port = args.port || process.env.PORT || 5555
+const debug = args.debug || false
+//const log = args.log || true
+console.log(debug)
+//args[("port", "debug", "log", "help")];
+
+const help = `
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`
 
 if (args.help || args.h) {
-  const help = `
-    server.js [options]
-
-    --port	Set the port number for the server to listen on. Must be an integer
-                between 1 and 65535.
-
-    --debug	If set to true, creates endlpoints /app/log/access/ which returns
-                a JSON access log from the database and /app/error which throws 
-                an error with the message "Error test successful." Defaults to 
-                false.
-
-    --log		If set to false, no log files are written. Defaults to true.
-                Logs are always written to database.
-
-    --help	Return this message and exit.
-    `;
-  console.log(help);
-  process.exit(0);
+  console.log(help)
+  process.exit(0)
 }
 
-if (log == true) {
-  const accesslog = fs.createWriteStream("access.log", { flags: "a" });
-  app.use(morgan("combined", { stream: accesslog }));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json)
+
+const server = app.listen(port, () => {
+  console.log("App is running on port %PORT%".replace("%PORT%", port))
+})
+
+app.get("/app/", (req, res) => {
+  // Respond with status 200
+  res.statusCode = 200
+  // Respond with status message "OK"
+  res.statusMessage = "OK"
+  res.writeHead(res.statusCode, {"Content-Type": "text/plain"})
+  res.end(res.statusCode + " " + res.statusMessage)
+})
+
+if (args.log === "true") {
+  const accesslog = fs.createWriteStream("access.log", {flags: "a"})
+  app.use(morgan("combined", {stream: accesslog}))
 } else {
-  console.log("Log file not created");
+  console.log("Log file not created")
 }
 
 app.use((req, res, next) => {
@@ -53,12 +68,12 @@ app.use((req, res, next) => {
     httpversion: req.httpVersion,
     status: res.statusCode,
     referer: req.headers["referer"],
-    useragent: req.headers["user-agent"],
-  };
-  console.log(logdata);
+    useragent: req.headers["user-agent"]
+  }
+  console.log(logdata)
   const stmt = logdb.prepare(
-    "INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
-  );
+    `INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
+  )
   const info = stmt.run(
     logdata.remoteaddr,
     logdata.remoteuser,
@@ -70,106 +85,88 @@ app.use((req, res, next) => {
     logdata.status,
     logdata.referer,
     logdata.useragent
-  );
-  next();
-});
+  )
+  next()
+})
+
+app.get("/app/flip/", (req, res, next) => {
+  var flip = coinFlip()
+  res.status(200).json({flip: flip})
+})
+
+app.get("/app/flips/:number", (req, res, next) => {
+  var flips = coinFlips(req.params.number)
+  res.status(200).json({raw: flips, summary: countFlips(flips)})
+})
+
+app.get("/app/flip/call/heads", (req, res, next) => {
+  var heads = flipACoin("heads")
+  res.status(200).json(heads)
+})
+
+app.get("/app/flip/call/tails", (req, res, next) => {
+  var tails = flipACoin("tails")
+  res.status(200).json(tails)
+})
+
+if (debug === true) {
+  app.get("/app/log/access", (req, res, next) => {
+    try {
+      const stmt = logdb.prepare("SELECT * FROM accesslog").all()
+      res.status(200).json(stmt)
+    } catch {
+      console.error(e)
+    }
+  })
+
+  app.get("/app/error", (req, res, next) => {
+    throw new Error("Error test successful.")
+  })
+}
+
+app.use(function (req, res, next) {
+  res.status(404).send("404 NOT FOUND")
+  res.type("text/plain")
+})
 
 function coinFlip() {
-  return Math.random() > 0.5 ? "heads" : "tails";
+  return Math.random() > 0.5 ? "heads" : "tails"
 }
 
 function coinFlips(flips) {
-  var temp = new Array();
+  var temp = new Array()
   if (flips < 1 || typeof flips == "undefined") {
-    flips = 1;
+    flips = 1
   }
   for (var i = 0; i < flips; i++) {
     //var coin = Math.random > .5 ? ("heads") : ("tails");
 
-    temp.push(coinFlip());
+    temp.push(coinFlip())
   }
-  return temp;
+  return temp
 }
 
 function countFlips(array) {
-  let h = 0;
-  let t = 0;
+  let h = 0
+  let t = 0
   for (let i = 0; i < array.length; i++) {
     if (array[i] == "heads") {
-      h++;
+      h++
     }
     if (array[i] == "tails") {
-      t++;
+      t++
     }
   }
-  return { heads: h, tails: t };
+  return {heads: h, tails: t}
 }
 
 function flipACoin(call) {
-  let flip = coinFlip();
-  let result = "";
+  let flip = coinFlip()
+  let result = ""
   if (flip == call) {
-    result = "win";
+    result = "win"
   } else {
-    result = "lose";
+    result = "lose"
   }
-  return { call: call, flip: flip, result: result };
+  return {call: call, flip: flip, result: result}
 }
-
-if (debug == true) {
-  app.get("/app/log/access/", (req, res) => {
-    try {
-      const stmt = logdb.prepare("SELECT * FROM accesslog").all();
-      res.status(200).json(stmt);
-    } catch {
-      console.error(e);
-    }
-  });
-
-  app.get("/app/error", (req, res) => {
-    throw new Error("Error test successful.");
-  });
-}
-
-const server = app.listen(port, "localhost", () => {
-  console.log("App is running on port %PORT%".replace("%PORT%", port));
-});
-
-app.get("/app/", (req, res) => {
-  // Respond with status 200
-  res.statusCode = 200;
-  // Respond with status message "OK"
-  res.statusMessage = "OK";
-  res.writeHead(res.statusCode, { "Content-Type": "text/plain" });
-  res.end(res.statusCode + " " + res.statusMessage);
-});
-
-app.get("/app/flip/", (req, res) => {
-  var flip = coinFlip();
-  res.status(200).json({ flip: flip });
-});
-
-app.get("/app/flips/:number", (req, res) => {
-  var flips = coinFlips(req.params.number);
-  res.status(200).json({ raw: flips, summary: countFlips(flips) });
-});
-
-app.get("/app/flip/call/heads", (req, res) => {
-  var heads = flipACoin("heads");
-  res.status(200).json(heads);
-});
-
-app.get("/app/flip/call/tails", (req, res) => {
-  var tails = flipACoin("tails");
-  res.status(200).json(tails);
-});
-
-app.use(function (req, res) {
-  res.status(404).send("404 NOT FOUND");
-});
-
-process.on("SIGTERM", () => {
-  server.close(() => {
-    console.log("Server stopped");
-  });
-});
